51e26deb073341eea9ef620158faae5cb0aee81b
davood-dorostkar <dado2hacker@gmail.com>
2023-10-19 18:08:21 +0330

    add: some basic parts of uri specifications

---
+++ src/ara/rest/Uri.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+
+namespace ara
+{
+    namespace rest
+    {
+        class Uri
+        {
+        public:
+            Uri(const std::string &path) noexcept : path(path) {}
+
+            class Builder
+            {
+            private:
+                std::string path;
+
+            public:
+                Builder() = default;
+                Builder &setPath(const std::string &path);
+                Uri build() const;
+            };
+
+        private:
+            std::string path;
+            std::string percentEncode(const std::string &input) noexcept; // must be changed regarding SWS_REST_01103
+            std::string percentDecode(const std::string &input) noexcept;
+        };
+
+    }
+
+}
---
+++ src/ara/rest/server.h
@@ -0,0 +1,22 @@
+#ifndef ARASERVER_H
+#define ARASERVER_H
+
+namespace ara
+{
+    namespace rest
+    {
+        class Server
+        {
+        private:
+        public:
+            Server();
+            ~Server();
+        };
+
+        Server::Server() {}
+        Server::~Server() {}
+
+    } // namespace rest
+
+} // namespace ara
+#endif
